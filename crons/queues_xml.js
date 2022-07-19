5ea0e49e41683dc0610317ab895d80c7db5338fc
Renan Nunes <renan.dev@hotmail.com>
2022-07-19 09:09:29 -0300

    Queues 2

--- crons/queues_xml.js
+++ crons/queues_xml.js
@@ -9,12 +9,17 @@ let url = process.env.TYPE_SERVER == 'local' ? `${process.env.HOST}:${process.en
 
 
 cron.schedule('*/60 * * * * *', async () => {
+
+	console.log(`add queue: start: ${process.env.CRON_HOUR_START} - end: ${process.env.CRON_HOUR_END}`)
+
 	if(parseInt(moment().format("HH")) > process.env.CRON_HOUR_START && parseInt(moment().format("HH")) < process.env.CRON_HOUR_END){
 	
 		if(parseInt(moment().format("mm")) < 20 && parseInt(moment().format("mm")) > 0){
 			
 			try{
-			
+				console.log()
+				console.log('entrou na cron do axios')
+				console.log()
 				await axios.get(`${url}/xml/add_to_queue`).then(resp => {
 					console.log(resp.data)
 				}).catch(async error => {
@@ -22,7 +27,7 @@ cron.schedule('*/60 * * * * *', async () => {
 				})
 				
 			}catch(e){
-				
+				console.log(e)
 			}
 		}else{
 			await redis_client.set('logs_crons:aviso:queue_xml', 'Fora do horário de baixar novos XMLs. Minuto atual: '+moment().format("mm"), 60)
