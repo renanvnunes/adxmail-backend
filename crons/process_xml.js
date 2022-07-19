5d2a7175e2b8e61a683414365e5e01d943de2458
Renan Nunes <renan.dev@hotmail.com>
2022-07-19 10:53:57 -0300

    Queues 6

--- crons/process_xml.js
+++ crons/process_xml.js
@@ -7,8 +7,8 @@ let url = process.env.TYPE_SERVER == 'local' ? `${process.env.HOST}:${process.en
 
 // 30
 
-cron.schedule('*/500 * * * * *', async () => {
-	console.log(`process: start: ${process.env.CRON_HOUR_START} - end: ${process.env.CRON_HOUR_END}`)
+cron.schedule('*/30 * * * * *', async () => {
+	
 	if(parseInt(moment().format("HH")) > process.env.CRON_HOUR_START && parseInt(moment().format("HH")) < process.env.CRON_HOUR_END){
 
 		if(parseInt(moment().format("mm")) > 25 && parseInt(moment().format("mm")) < 59){
@@ -16,8 +16,6 @@ cron.schedule('*/500 * * * * *', async () => {
 			try{
 
 				await axios.get(`${url}/xml/process_queue`).then(resp => {
-
-					console.log(resp.data)
 					
 				}).catch(async error => {
 					await redis_client.set('logs_crons:erro:process_queue', 'Erro ao rodar a cron de (process_queue) - ' + error, 60)
