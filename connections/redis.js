9ca9d731119b9f9055c9717994968c68a083d31f
Renan Nunes <77364643+renanvnunes@users.noreply.github.com>
2022-10-13 10:16:42 -0300

    Update redis.js

--- connections/redis.js
+++ connections/redis.js
@@ -13,16 +13,6 @@ class ClassRedis{
 				host: process.env.RD_HOST
 			}
 		})
-		
-		console.log({
-			host      : process.env.RD_HOST,  
-			port      : Number(process.env.RD_PORT),
-			password  : process.env.RD_PASS,
-			keyPrefix : `${process.env.RD_PREFIX}:`,
-			tls: {
-				host: process.env.REDIS_HOST
-			}
-		})
 	}
 
 	async get_all(search)
