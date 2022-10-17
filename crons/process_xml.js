430c73c13662e3b31a1c9d15cc87a0b642460c4c
Renan Vinicius <renan.dev@hotmail.com>
2022-10-17 08:19:53 -0300

    Change html

--- crons/process_xml.js
+++ crons/process_xml.js
@@ -7,7 +7,7 @@ let url = process.env.TYPE_SERVER == 'local' ? `${process.env.HOST}:${process.en
 
 // 30
 
-cron.schedule('*/30 * * * * *', async () => {
+cron.schedule('*/60 * * * * *', async () => {
 	
 	if(parseInt(moment().format("HH")) > process.env.CRON_HOUR_START && parseInt(moment().format("HH")) < process.env.CRON_HOUR_END){
 
