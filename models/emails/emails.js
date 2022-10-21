1f04ab5808559319f468c5b1db89e8469fa1b546
Renan Vinicius <renan.dev@hotmail.com>
2022-10-21 20:39:11 -0300

    Changes

--- models/emails/emails.js
+++ models/emails/emails.js
@@ -5,6 +5,9 @@ const Schema = mongoose.Schema
 const default_date = moment().format('YYYY-MM-DD HH:mm:ss')
 // const default_date = moment().toISOString("YYYY-MM-DD HH:mm:ss")
 
+
+
+
 const emailsSchema = new Schema({
 	migrado: {
 		type: Boolean,
