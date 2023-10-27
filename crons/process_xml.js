fdd357edf4237037eaa6e38a248d9de14323eefb
Renan Nunes <renan.dev@hotmail.com>
2023-10-27 09:07:11 -0300

    Devolvendo fila de crons

--- crons/process_xml.js
+++ crons/process_xml.js
@@ -9,25 +9,27 @@ let url = process.env.TYPE_SERVER == 'local' ? `${process.env.HOST}:${process.en
 
 cron.schedule('*/240 * * * * *', async () => {
 	// if(parseInt(moment().format("HH")) > process.env.CRON_HOUR_START && parseInt(moment().format("HH")) < process.env.CRON_HOUR_END){
-	// if (parseInt(moment().format('mm')) > 25 && parseInt(moment().format('mm')) < 59) {
-	// 	try {
-	// 		await axios
-	// 			.get(`${url}/xml/process_queue`)
-	// 			.then((resp) => {})
-	// 			.catch(async (error) => {
-	// 				await redis_client.set(
-	// 					'logs_crons:erro:process_queue',
-	// 					'Erro ao rodar a cron de (process_queue) - ' + error,
-	// 					60
-	// 				);
-	// 			});
-	// 	} catch (e) {}
-	// } else {
-	// 	await redis_client.set(
-	// 		'logs_crons:aviso:process_xml',
-	// 		'Fora do horário de atualização de produtos. Minuto atual: ' + moment().format('mm'),
-	// 		60
-	// 	);
-	// }
+
+	if (parseInt(moment().format('mm')) > 25 && parseInt(moment().format('mm')) < 59) {
+		try {
+			await axios
+				.get(`${url}/xml/process_queue`)
+				.then((resp) => {})
+				.catch(async (error) => {
+					await redis_client.set(
+						'logs_crons:erro:process_queue',
+						'Erro ao rodar a cron de (process_queue) - ' + error,
+						60
+					);
+				});
+		} catch (e) {}
+	} else {
+		await redis_client.set(
+			'logs_crons:aviso:process_xml',
+			'Fora do horário de atualização de produtos. Minuto atual: ' + moment().format('mm'),
+			60
+		);
+	}
+
 	// }
 });
--- crons/queues_xml.js
+++ crons/queues_xml.js
@@ -12,25 +12,25 @@ cron.schedule('*/200 * * * * *', async () => {
 		parseInt(moment().format('HH')) > process.env.CRON_HOUR_START &&
 		parseInt(moment().format('HH')) < process.env.CRON_HOUR_END
 	) {
-		// if (parseInt(moment().format('mm')) < 20 && parseInt(moment().format('mm')) > 0) {
-		// 	try {
-		// 		await axios
-		// 			.get(`${url}/xml/add_to_queue`)
-		// 			.then((resp) => {})
-		// 			.catch(async (error) => {
-		// 				await redis_client.set(
-		// 					'logs_crons:erro:queue_xml',
-		// 					'Erro ao rodar a cron de (add_to_queue) - ' + error,
-		// 					60
-		// 				);
-		// 			});
-		// 	} catch (e) {}
-		// } else {
-		// 	await redis_client.set(
-		// 		'logs_crons:aviso:queue_xml',
-		// 		'Fora do horário de baixar novos XMLs. Minuto atual: ' + moment().format('mm'),
-		// 		60
-		// 	);
-		// }
+		if (parseInt(moment().format('mm')) < 20 && parseInt(moment().format('mm')) > 0) {
+			try {
+				await axios
+					.get(`${url}/xml/add_to_queue`)
+					.then((resp) => {})
+					.catch(async (error) => {
+						await redis_client.set(
+							'logs_crons:erro:queue_xml',
+							'Erro ao rodar a cron de (add_to_queue) - ' + error,
+							60
+						);
+					});
+			} catch (e) {}
+		} else {
+			await redis_client.set(
+				'logs_crons:aviso:queue_xml',
+				'Fora do horário de baixar novos XMLs. Minuto atual: ' + moment().format('mm'),
+				60
+			);
+		}
 	}
 });
